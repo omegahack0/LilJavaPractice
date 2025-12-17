e576971a3f3b656bd1a00fc35d44839efcb1dbe5
omegahack0 <matthewportermcdowell@gmail.com>
2025-12-17 10:34:57 -0500

    doing some cleanup like a gangster

--- src/FindingPrimeTest.java
+++ src/FindingPrimeTest.java
@@ -2,9 +2,12 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class FindingPrimeTest {
+    Startup startupClass = new Startup();
+    int kaprekarConstant = 6174;
 
     @Test
     public void testFindingFactorsCorrectForSix(){
@@ -16,7 +19,31 @@ public class FindingPrimeTest {
         factorsOfSix.add(6);
 
         //mock the function call
-        LinkedList<Integer> result = main.findFactors(6);
+        ArrayList<Integer> result = startupClass.findFactors(6);
+
+        // Verify we are getting the correct values
+        Assert.assertEquals(factorsOfSix, result);
+    }
+
+    @Test
+    public void testFindingFactorsCorrectForSixty(){
+        //Creating a version of the object we are expecting back
+        LinkedList<Integer> factorsOfSix = new LinkedList<>();
+        factorsOfSix.add(1);
+        factorsOfSix.add(2);
+        factorsOfSix.add(3);
+        factorsOfSix.add(4);
+        factorsOfSix.add(5);
+        factorsOfSix.add(6);
+        factorsOfSix.add(10);
+        factorsOfSix.add(12);
+        factorsOfSix.add(15);
+        factorsOfSix.add(20);
+        factorsOfSix.add(30);
+        factorsOfSix.add(60);
+
+        //mock the function call
+        ArrayList<Integer> result = startupClass.findFactors(60);
 
         // Verify we are getting the correct values
         Assert.assertEquals(factorsOfSix, result);
@@ -33,7 +60,7 @@ public class FindingPrimeTest {
         oddNumbers.add(9);
 
         //mock the function call
-        LinkedList<Integer> result = main.findOddNumber(5);
+        LinkedList<Integer> result = startupClass.findOddNumber(5);
 
         // Verify we are getting expected values
         Assert.assertEquals(oddNumbers, result);
@@ -50,7 +77,7 @@ public class FindingPrimeTest {
         primeNumbers.add(7);
 
         //mock the function call
-        LinkedList<Integer> result = main.findPrimeDigit(5);
+        LinkedList<Integer> result = startupClass.findPrimeDigit(5);
 
         // Verify we are getting expected values
         Assert.assertEquals(primeNumbers, result);
@@ -66,7 +93,7 @@ public class FindingPrimeTest {
         perfectNumbers.add(8128);
 
         //mock the function call
-        LinkedList<Integer> result = main.findPerfectNumber(4);
+        ArrayList<Integer> result = startupClass.findPerfectNumber(4);
 
         // Verify we are getting expected values
         Assert.assertEquals(result, perfectNumbers);
@@ -75,14 +102,14 @@ public class FindingPrimeTest {
     @Test
     public void testIsPerfectTrue(){
         //Creating a version of the object we are expecting back
-        LinkedList<Integer> perfectNumberFactors = new LinkedList<>();
+        ArrayList<Integer> perfectNumberFactors = new ArrayList<>();
         perfectNumberFactors.add(1);
         perfectNumberFactors.add(2);
         perfectNumberFactors.add(3);
         perfectNumberFactors.add(6);
 
         //mock the function call
-        boolean result = main.isPerfect(perfectNumberFactors,6);
+        boolean result = startupClass.isPerfect(perfectNumberFactors,6);
 
         // Verify we are getting expected values
         Assert.assertTrue(result);
@@ -91,7 +118,7 @@ public class FindingPrimeTest {
     @Test
     public void testIsPerfectFalse(){
         //Creating a version of the object we are expecting back
-        LinkedList<Integer> perfectNumberFactors = new LinkedList<>();
+        ArrayList<Integer> perfectNumberFactors = new ArrayList<>();
         perfectNumberFactors.add(1);
         perfectNumberFactors.add(2);
         perfectNumberFactors.add(3);
@@ -102,7 +129,7 @@ public class FindingPrimeTest {
         perfectNumberFactors.add(24);
 
         //mock the function call
-        boolean result = main.isPerfect(perfectNumberFactors,24);
+        boolean result = startupClass.isPerfect(perfectNumberFactors,24);
 
         // Verify we are getting expected values
         Assert.assertFalse(result);
@@ -110,7 +137,8 @@ public class FindingPrimeTest {
 
     @Test
     public void testFindKaprikarsConstant(){
-        Assert.assertEquals(6174,main.findKapricoreConstant(1234));
+        Assert.assertEquals(kaprekarConstant,
+                startupClass.findKaprekarConstant(1234));
     }
 
 
--- src/Startup.java
+++ src/Startup.java
@@ -1,19 +1,33 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 
-class main {
-    public static void main(String args[]) {
+//TODO play with setting up/sorting an arraylist
+    //Chosing Arraylist because it appears to be be the best of both worlds, better than LL or Array
+//TODO mess with java streams/filters
+//TODO Refactor to se arraylist instead of LL
+//TODO use either int or Integer but not both
+//TODO not everything should be a static context -- I think its fine in this instance though
+    //Rule of thumb
+    //If its not a utility class or it needs No access to instance data it shouldn't be static
+    //https://stackoverflow.com/questions/2671496/when-to-use-static-methods
+    //Static means that the method or variable does not belong to any instance of the class but rather to the class itself. You use it if, and only if, you need a method or variable to be wholly decoupled from the objects of the class, such as for global variables or some utility methods. Such a situation isn't normally that common, which is why you don't normally use the static keyword that often.
+
+
+class Startup {
+    public static void main(String[] args) {
+        Startup startClass = new Startup();
         System.out.println("Amount of Odd Numbers");
-        System.out.println(findOddNumber(1000));
+        System.out.println(startClass.findOddNumber(1000));
         System.out.println("Find Prime Numbers");
-        System.out.println(findPrimeDigit(1000));
+        System.out.println(startClass.findPrimeDigit(1000));
         System.out.println("Find Perfect Numbers");
-        System.out.println(findPerfectNumber(10));
+        System.out.println(startClass.findPerfectNumber(10));
         System.out.println("Find Kapricore Constant");
-        System.out.println(findKapricoreConstant(0124));
+        System.out.println(startClass.findKaprekarConstant(0124));
 
     }
 
-    public static LinkedList<Integer> findOddNumber(int amountOfOddNumbers){
+    public LinkedList<Integer> findOddNumber(int amountOfOddNumbers){
         LinkedList<Integer> OddNumbers = new LinkedList<>();
         int i = 0;
         while(amountOfOddNumbers > OddNumbers.size()){
@@ -27,7 +41,7 @@ class main {
         return OddNumbers;
     }
 
-    public static LinkedList<Integer> findPrimeDigit(int amountOfPrimeNumbers){
+    public LinkedList<Integer> findPrimeDigit(int amountOfPrimeNumbers){
         LinkedList<Integer> PrimeNumbers = new LinkedList<>();
         int i = 0;
         while(amountOfPrimeNumbers > PrimeNumbers.size()){
@@ -42,8 +56,9 @@ class main {
         return PrimeNumbers;
     }
 
-    public static LinkedList<Integer> findPerfectNumber(int amountOfPerfectNumbers){
-        LinkedList<Integer> PerfectNumbers = new LinkedList<>();
+    public ArrayList<Integer> findPerfectNumber(int amountOfPerfectNumbers){
+        //TODO SPEED UP finding more Perfect numbers because its dog slow
+        ArrayList<Integer> PerfectNumbers = new ArrayList<>();
         int i = 1;
         while(amountOfPerfectNumbers > PerfectNumbers.size()){
             // Perfect Number - a number that adds up to all its
@@ -51,15 +66,15 @@ class main {
             // 6 has proper divisors 1, 2 and 3, and 1 + 2 + 3 = 6, so 6 is a perfect number.
 
             if(isPerfect(findFactors(i),i))
-            PerfectNumbers.add(i);
+                PerfectNumbers.add(i);
 
             i++;
         }
         return PerfectNumbers;
     }
 
-    public static LinkedList<Integer> findFactors(int numberToBeFactored){
-        LinkedList<Integer> factorsOfNumber = new LinkedList<>();
+    public ArrayList<Integer> findFactors(int numberToBeFactored){
+        ArrayList<Integer> factorsOfNumber = new ArrayList<>();
 
         //Might as well just divide by 2 and see if its a whole number
         //Would depend on if there is any speed improvement for modules operator vs just dividing
@@ -74,7 +89,7 @@ class main {
         return factorsOfNumber;
     }
 
-    public static boolean isPerfect(LinkedList<Integer> factorsOfPotentiallyPerfectNumber, int potentiallyPerfectNumber){
+    public boolean isPerfect(ArrayList<Integer> factorsOfPotentiallyPerfectNumber, int potentiallyPerfectNumber){
         int tmp = 0;
         factorsOfPotentiallyPerfectNumber.removeLast();
 
@@ -91,13 +106,17 @@ class main {
         return false;
     }
 
-    public static int findKapricoreConstant(Integer numberToCalculateKapricoreConstant){
+    public int findKaprekarConstant(Integer numberToCalculateKaprekarConstant){
         //TODO implement
+        //TODO add some debug logging too because it would be cool
+        //TODO make recursive
+        //TODO reach constant without keeping constant stored in mem ie dont have a int to check against?
+            //Maybe if we get the same value multiple times in a row
         //Error checking for 4 digit number? and repdigit?
         int KapricoreConstant = 6174;
 
         //Rearrange the digits in the number from largest to smallest ie 1234 to 4321
-        String stringToCalcualteConstant = numberToCalculateKapricoreConstant.toString();
+        String stringToCalcualteConstant = numberToCalculateKaprekarConstant.toString();
 
         //Sort the string array
         stringToCalcualteConstant.chars();
